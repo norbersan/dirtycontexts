2d87077298a4e46f4a4d3e8cefbb6bdc645925fb
norber_san <norber.sanchezpuertas@aldautomotive.com>
2021-10-28 09:21:17 +0200

    setup empty project

---
+++ src/main/kotlin/com/norbersan/dirtycontexts/DirtycontextsApplication.kt
@@ -0,0 +1,11 @@
+package com.norbersan.dirtycontexts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DirtycontextsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DirtycontextsApplication>(*args)
+}
---
+++ src/test/kotlin/com/norbersan/dirtycontexts/DirtycontextsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.norbersan.dirtycontexts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DirtycontextsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
